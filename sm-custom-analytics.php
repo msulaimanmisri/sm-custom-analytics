fa6f4bcc22e01c262e52a256212b7a47f69a45ec
Sulaiman Misri <msulaimanmisri@gmail.com>
2023-08-22 17:34:12 +0800

    Initialize

---
+++ sm-custom-analytics.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin Name: SM Custom Analytics
+ * Plugin URI: https://github.com/msulaimanmisri/sm-custom-analytics
+ * Description: work in progress
+ * Version: 1.0
+ * Author: Sulaiman Misri
+ * Author URI: https://github.com/msulaimanmisri
+ * Text Domain: sm-custom-analytics
+ * Domain Path: /languages
+ * Tested up to: 6.1.1
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+require 'Controller/AnalyticsController.php';
